ecc0f7cf15b51e25a67b0462cd44eee947564fe9
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-11-23 15:39:49 +0530

    Nov 23

---
+++ 2501-3000/leetcode2859.java
@@ -0,0 +1,16 @@
+
+DATE : 23-NOV-2025
+LEVEL : Easy
+TOPIC : Bit Manipulation
+
+class Solution {
+    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            if(Integer.bitCount(i)==k){
+                sum+=nums.get(i);
+            }
+        }
+        return sum;
+    }
+}
