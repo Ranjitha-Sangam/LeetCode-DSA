f3770e478603b32e89b7cff973ffc1d6d95c3a50
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-12-01 12:23:13 +0530

    dec 1

--- 1 to 500/leetcode111.java
+++ 1 to 500/leetcode111.java
@@ -10,10 +10,19 @@ class Solution {
         }
         int leftHeight=minDepth(root.left);
         int rightHeight=minDepth(root.right);
-
+        //when one child is null (spcl conditions)
         if (root.left == null) return rightHeight + 1;
         if (root.right == null) return leftHeight + 1;
 
         return Math.min(leftHeight,rightHeight)+1;
     }
 }
+
+
+We dnt use  Math.min() when one child is null.
+for ex : 1   3
+        /     \
+       2       5
+       in these conditions we dnt use Math.min()
+
+    answer is returnd from one of the 2 spcl conditons
