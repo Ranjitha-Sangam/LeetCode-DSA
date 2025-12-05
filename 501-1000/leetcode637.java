1f19110fce49700fb658f3d68026e38d80b1a363
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-12-05 19:08:53 +0530

    dec 5

---
+++ 501-1000/leetcode637.java
@@ -0,0 +1,32 @@
+
+DATE : 05-DEC-2025
+LEVEL : Easy
+TOPIC : Binary Tree BFS
+
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans = new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            double sum=0;
+            for(int i=0;i<size;i++){
+                TreeNode node = q.poll();
+                sum+=node.val;
+
+                if(node.left!=null){
+                    q.add(node.left);
+                }
+                if(node.right!=null){
+                    q.add(node.right);
+                }
+            }
+            ans.add(sum/size);
+        }
+        return ans;
+    }
+}
