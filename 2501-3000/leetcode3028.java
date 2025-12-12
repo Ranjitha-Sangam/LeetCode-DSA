faa12d76b054909ec5ec1b929da5e50ad8b6f24e
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-12-12 18:19:43 +0530

    dec 12

---
+++ 2501-3000/leetcode3028.java
@@ -0,0 +1,16 @@
+
+DATE :12-DEC-2025
+LEVEL : Easy
+TOPIC : Array
+
+class Solution {
+    public int returnToBoundaryCount(int[] nums) {
+        int boundary=0;
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(sum==0) boundary++;
+        }
+        return boundary;
+    }
+}
