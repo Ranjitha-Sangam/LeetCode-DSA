13af3905979ff82307d3436bba1fc02a940bdacd
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-12-16 15:42:22 +0530

    dec 16

---
+++ 2000-2500/leetcode2169.java
@@ -0,0 +1,19 @@
+
+DATE : 16-DEC-2025
+LEVEL : Easy
+TOPIC : Math
+
+class Solution {
+    int count=0;
+    public int countOperations(int num1, int num2) {
+        while(num1!=0 && num2!=0){
+        if(num1<num2){
+            num2-=num1;
+        }else{
+            num1-=num2;
+        }
+        count++ ;
+        }
+        return count;
+    }
+}
