e4981b50aa8e4a5ce7d1a9bed20a8062b4a4aa12
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-12-19 14:20:40 +0530

    dec 19

---
+++ 501-1000/leetcode653.java
@@ -0,0 +1,18 @@
+
+DATE : 19-DEC-2025
+LEVEL : Easy
+TOPIC : Binary Search Tree and HashSet
+
+class Solution {
+    HashSet<Integer> set = new HashSet<>();
+    public boolean findTarget(TreeNode root, int k) {
+        if(root==null){
+            return false;
+        }
+        if(set.contains(k-root.val)){
+            return true;
+        }
+        set.add(root.val);
+        return findTarget(root.left,k)||findTarget(root.right,k);
+    }
+}
