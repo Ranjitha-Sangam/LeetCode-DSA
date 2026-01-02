deb294a6b9d0a705477cbdc02c570117588735dd
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-01-02 21:34:57 +0530

    jan 2

---
+++ 2501-3000/leetcode2558.java
@@ -0,0 +1,25 @@
+
+DATE : 02-JAN-2025
+LEVEL : Easy
+TOPIC : PriorityQueue
+
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+        long sum=0;
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        for(int g:gifts){
+            pq.offer(g);
+        }
+        int count=0;
+        while(count<k){
+            int max=pq.poll();
+            int reducedGifts=(int)Math.sqrt(max);
+            pq.offer(reducedGifts);
+            count++;
+        }
+        while(!pq.isEmpty()){
+            sum+=pq.poll();
+        }
+        return sum;
+    }
+}
