6ecb0770e65a39d380c79d224f39fe2e380dc3f8
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-01-03 19:29:38 +0530

    jan 3

---
+++ 1501-2000/leetcode1929.java
@@ -0,0 +1,16 @@
+
+DATE : 03-JAN-2026
+LEVEL : Easy
+TOPIC : Arrays
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n=nums.length;
+        int ans[]=new int[2*n];
+        for(int i=0;i<n;i++){
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+        }
+        return ans;
+    }
+}
