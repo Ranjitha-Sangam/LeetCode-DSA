de6dbc0829652d69506cdf4c1cd9d5c64bc165dd
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-01-18 19:14:56 +0530

    jan 18

---
+++ 1501-2000/leetcode1941.java
@@ -0,0 +1,24 @@
+
+DATE : 18-JAN-2026
+LEVEL : Easy
+TOPIC : String 
+
+class Solution {
+    public boolean areOccurrencesEqual(String s) {
+        int freq[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
+        }
+        int expected=0;
+        for(int f:freq){
+            if(f!=0){
+                if(expected==0){
+                    expected=f;
+                }else if(f!=expected){
+                    return false;
+                }
+            }
+        }
+       return true;
+    }
+}
