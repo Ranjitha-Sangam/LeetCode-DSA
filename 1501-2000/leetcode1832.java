81aa87ec0cd02abe43484c911eec12aa02c66fe5
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-01-23 19:18:58 +0530

    jan 23

---
+++ 1501-2000/leetcode1832.java
@@ -0,0 +1,20 @@
+
+DATE : 23-JAN-2026
+LEVEL : Easy
+TOPIC : String 
+
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int freq[]=new int[26];
+        for(int i=0;i<sentence.length();i++){
+            char ch=sentence.charAt(i);
+            freq[ch-'a']++;
+        }
+        for(int i=0;i<freq.length;i++){
+            if(freq[i]==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
