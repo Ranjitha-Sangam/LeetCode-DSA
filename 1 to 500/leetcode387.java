19fa01edd9431e9088ee30fb0fba1d964f418579
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-02-03 15:45:53 +0530

    nov 13 & feb 3

--- 1 to 500/leetcode387.java
+++ 1 to 500/leetcode387.java
@@ -17,3 +17,21 @@ class Solution {
         return -1;
     }
 }
+
+//USING HASHMAP
+
+class Solution{
+    public int firstUniqChar(String s){
+        HashMap<Character,Integer> map=new HashMap<>();
+        for(char c: s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(int i=0;i<s.length();i++){
+            if(map.get(s.charAt(i))==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
