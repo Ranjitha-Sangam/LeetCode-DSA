81fdc3a4d6cd0e84152ac5e4c4ad0176b5ca92ec
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-02-04 08:06:22 +0530

    oct 2 & feb 4

--- 1 to 500/leetcode169.java
+++ 1 to 500/leetcode169.java
@@ -17,3 +17,22 @@ class Solution {
         
     }
 }
+
+
+DATE : 04-FEB-2026
+LEVEL : Easy
+TOPIC : HASHMAP
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+            if(map.get(num)>n/2){
+                return num;
+            }
+        }
+        return -1;
+    }
+}
