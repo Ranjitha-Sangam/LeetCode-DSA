080c1598dd850d0cd0059363165a0f437ffb3b27
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-02-06 08:16:50 +0530

    oct 1 & feb 6

--- 1 to 500/leetcode136.java
+++ 1 to 500/leetcode136.java
@@ -13,3 +13,24 @@ class Solution {
         return res;   
     }
 }
+
+
+DATE : 06-FEB-2026
+LEVEL : Easy
+TOPIC : HashSet
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int n:nums){
+            if(set.contains(n)){
+                set.remove(n);
+            }else{
+                set.add(n);
+            }
+        }
+        return set.iterator().next();
+    }
+}
+// Every pair gets added then removed
+// Only the single number is left,thats y we did return set.iterator().next();
