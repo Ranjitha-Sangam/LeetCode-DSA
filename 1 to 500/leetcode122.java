423561802e13f4ef6038ef27ee0788572b78a616
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-02-28 21:51:08 +0530

    feb 28

---
+++ 1 to 500/leetcode122.java
@@ -0,0 +1,16 @@
+
+DATE : 28-FEB-2026
+LEVEL : Easy
+TOPIC : Array
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]>prices[i-1]){
+                profit+=prices[i]-prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
