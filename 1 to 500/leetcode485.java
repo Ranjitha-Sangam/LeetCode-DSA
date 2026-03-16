301043132870c74561e76c2d689b5d9324bc95b8
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-03-16 21:18:11 +0530

    march 16

---
+++ 1 to 500/leetcode485.java
@@ -0,0 +1,21 @@
+
+DATE : 16-March-2026
+LEVEL : Easy
+TOPIC : Arrays
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int n=nums.length;
+        int count=0;
+        int maxCount=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=1){
+                count=0;
+            }else{
+                count++;
+            }
+            maxCount=Math.max(count,maxCount);
+        }
+        return maxCount;
+    }
+}
