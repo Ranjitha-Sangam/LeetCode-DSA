8f034edbcbb213158766a72b167e2c3689197b5a
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-03-23 16:34:59 +0530

    march 23

---
+++ 1 to 500/leetcode144.java
@@ -0,0 +1,20 @@
+
+DATE : 23-March-2026
+LEVEL : Trees
+TOPIC : Binary Trees
+
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+        preorder(root,ans);
+        return ans;
+    }
+    private void preorder(TreeNode root,List<Integer> ans){
+        if(root==null){
+            return;
+        }
+        ans.add(root.val);
+        preorder(root.left,ans);
+        preorder(root.right,ans);
+    }
+}
