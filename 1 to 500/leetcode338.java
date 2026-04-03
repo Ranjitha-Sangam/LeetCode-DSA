182835e13955a493087d240a394da6f67153d418
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-04-03 14:36:48 +0530

    march 31

---
+++ 1 to 500/leetcode338.java
@@ -0,0 +1,14 @@
+
+DATE : 31-March-2026
+LEVEL : Easy
+TOPIC : Bit Manipulation
+
+class Solution {
+    public int[] countBits(int n) {
+        int res[]=new int[n+1];
+        for(int i=1;i<=n;i++){
+            res[i]=res[i>>1]+(i&1);
+        }
+        return res;
+    }
+}
