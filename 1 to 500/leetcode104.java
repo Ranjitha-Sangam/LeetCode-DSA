c958fc8aa08a06954488c654b6ab764de1e4bd6d
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-04-03 13:42:10 +0530

    march 25

---
+++ 1 to 500/leetcode104.java
@@ -0,0 +1,16 @@
+
+DATE : 25-March-2026
+LEVEL : Easy
+TOPIC : Binary Trees
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int leftHeight=maxDepth(root.left);
+        int rightHeight=maxDepth(root.right);
+        return Math.max(leftHeight,rightHeight)+1;
+        
+    }
+}
