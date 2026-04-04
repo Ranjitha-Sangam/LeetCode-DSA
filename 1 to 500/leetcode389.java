83190404030d4610ffc05838c8b63daed559b6cf
Ranjitha2710 <ranjithasangamss@gmail.com>
2026-04-04 20:28:18 +0530

    april 4

---
+++ 1 to 500/leetcode389.java
@@ -0,0 +1,20 @@
+
+DATE : 04-April-2026
+LEVEL : Easy
+TOPIC : HashMap
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        for(char c:t.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)-1);
+            if(map.get(c)<0){
+                return c;
+            }
+        }
+        return ' ';
+    }
+}
